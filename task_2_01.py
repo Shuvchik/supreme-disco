36d75817984edb8f3d7cc8172a7d7d5e403c528c
Shuvchik <ishuvs@gmail.com>
2023-01-06 18:47:42 +0300

    Task 2 1

---
+++ task_2_01.py
@@ -0,0 +1,15 @@
+
+# Напишите программу, которая принимает на вход вещественное число и показывает сумму его цифр. Учтите, что числа могут быть отрицательными
+# Пример:
+# 67.82 -> 23
+# 0.56 -> 11
+
+number = input('введите число')
+def sum_in_number(some_number):
+    sum = 0
+    for i in some_number:
+        if i.isdigit():
+            i = int(i)
+            sum = sum + i
+    print(sum)
+sum_in_number(number)
